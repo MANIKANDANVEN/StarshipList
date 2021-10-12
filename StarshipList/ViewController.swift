efac5030deba243e838ca90f14011f312c6d302f
Manikandan <Manikandanven@gmail.com>
2021-10-12 15:40:19 +1100

    Get Data From JSON

--- StarshipList/ViewController.swift
+++ StarshipList/ViewController.swift
@@ -8,10 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct mainData: Codable {
+        let count: Int
+        let next: String
+        let previous: String?
+        let results: [resultItem]
+    }
+    
+    struct resultItem: Codable {
+        let name: String
+        let model: String
+        let manufacturer: String
+        let cost_in_credits: String
+        let length: String
+        let max_atmosphering_speed: String
+        let crew: String
+        let passengers: String
+        let cargo_capacity: String
+        let consumables: String
+        let hyperdrive_rating: String
+        let MGLT: String
+        let starship_class: String
+        let pilots: [String]?
+        let films: [String]?
+        let created: String
+        let edited: String
+        let url: String
+    }
 
+    var starShipNameList: Array<String> = Array()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://swapi.dev/api/starships/?format=json") {
+           URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+        let jsonDecoder = JSONDecoder()
+        do {
+        let parsedJSON = try jsonDecoder.decode(mainData.self, from: data)
+            print (parsedJSON.count)
+            for item in parsedJSON.results {
+                print(item.name)
+                print(item.model)
+                    }
+                } catch {
+        print(error)
+                }
+               }
+           }.resume()
+        }
     }
 
 
